aee272ae26018ecb03fc4b7b16d1a163e7332c9d
Andrew Li <zeyu.li23@gmail.com>
2022-07-10 15:24:12 -0600

    fixed sticky

--- components/main/Story.js
+++ components/main/Story.js
@@ -38,19 +38,31 @@ const Story = () => {
   }, []);
 
   return (
-    <div className="story" id="story">
-      <div className="story__container">
-        <h2>Our Story</h2>
-        <div
-          ref={lottiee}
-          id="firstLottie"
-          controls
-          mode="scroll"
-          src={"data.json"}
-          style={{ width: "320px" }}
-        />
+    <>
+      <div className="story" id="story">
+        <div className="story__container">
+          <h2>Our Story</h2>
+        </div>
       </div>
-    </div>
+      <div>
+        {/* ref={intersectionRef}> */}
+        <div
+          className="story__container__video"
+          style={{ height: `${900 * 4}px` }}
+        >
+          <div style={{ position: "sticky", top: 0 }}>
+            <div
+              ref={lottiee}
+              className="lottie"
+              id="firstLottie"
+              controls
+              mode="scroll"
+              src={"data.json"}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Story;
